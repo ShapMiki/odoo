953f95d847d269ef3880241176a828aeee01a0cf
Mikita <Nikikiki01102005@gmail.com>
2025-08-20 15:52:16 +0300

    view

--- addons/library_management/__manifest__.py
+++ addons/library_management/__manifest__.py
@@ -7,6 +7,7 @@
     "data": [
         'security/library_security.xml',
         'security/ir.model.access.csv',
+        'views/library_views.xml',
     ],
     "installable": True,
     "application": True,
