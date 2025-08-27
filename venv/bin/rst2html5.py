58e9139110dadf8d8e1e5dad58f008c6aa69c7e9
Mikita <Nikikiki01102005@gmail.com>
2025-08-27 11:44:23 +0300

    fix roots

--- venv/bin/rst2html5.py
+++ venv/bin/rst2html5.py
@@ -1,4 +1,4 @@
-#!/home/mikita/project/python/odoo/venv/bin/python3
+#!/home/mikita/project/odoo/odoo/venv/bin/python3
 # :Copyright: © 2015 Günter Milde.
 # :License: Released under the terms of the `2-Clause BSD license`_, in short:
 #
